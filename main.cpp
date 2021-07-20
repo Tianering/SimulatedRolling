26e95f9504a4516efe90c21b7c6d808710dc66fc
tianering <Tianering@yeah.net>
2021-07-20 11:56:06 +0800

    主类_读取参数文件

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+#include "objectIR.h"
+#include "rolling.h"
+
 using namespace std;
 using namespace cv;
 
+
 int main() {
+    String path = "../config/config.txt";
+    Speckle_info speckleInfo;
+    Structure_info structureInfo;
+    Sensor_info sensorInfo;
+    Intrinsic intrinsic;
+    readTxt(speckleInfo,structureInfo,sensorInfo,intrinsic,path);
+
     Mat img = imread("../images/full_fov_speckle.bmp");
-    cout << "start" << endl;
+    cout << path << endl;
     return 0;
 }
