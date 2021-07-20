f0a1b21369f747b548811b43e7806af0b847edf1
tianering <Tianering@yeah.net>
2021-07-20 17:06:59 +0800

    主类_添加旋转

--- main.cpp
+++ main.cpp
@@ -14,9 +14,18 @@ int main() {
     Structure_info structureInfo;
     Sensor_info sensorInfo;
     Intrinsic intrinsic;
-    readTxt(speckleInfo,structureInfo,sensorInfo,intrinsic,path);
-
+    // 读取参数文件，获取相机内参等参数
+    readTxt(speckleInfo, structureInfo, sensorInfo, intrinsic, path);
     Mat img = imread("../images/full_fov_speckle.bmp");
-    cout << path << endl;
+
+    Mat img1 = Mat(img.rows, img.cols, CV_16FC3, Scalar(0, 0, 0));
+    // 右手坐标系
+    // 欧拉角为RPY角（roll、pitch、yaw），即xyz顺序
+    Vec3d euler_angles;
+    euler_angles[0] = 0;
+    euler_angles[1] = 1;
+    euler_angles[2] = 0;
+    img1 = rotateX(img, intrinsic, euler_angles);
+    imwrite("../images/rotated_y.png", img1);
     return 0;
 }
