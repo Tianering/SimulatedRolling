42267e1a564d2d8537d0226e341949a69f44cbb2
tianering <Tianering@yeah.net>
2021-07-21 15:34:14 +0800

    主类_添加距离调整功能

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/calib3d.hpp>
 
 #include "objectIR.h"
 #include "rolling.h"
@@ -16,16 +17,22 @@ int main() {
     Intrinsic intrinsic;
     // 读取参数文件，获取相机内参等参数
     readTxt(speckleInfo, structureInfo, sensorInfo, intrinsic, path);
+    cout << speckleInfo.distance << endl;
+    Vec3d translation;
+    // 平移向量
     Mat img = imread("../images/full_fov_speckle.bmp");
-
     Mat img1 = Mat(img.rows, img.cols, CV_16FC3, Scalar(0, 0, 0));
-    // 右手坐标系
-    // 欧拉角为RPY角（roll、pitch、yaw），即xyz顺序
-    Vec3d euler_angles;
-    euler_angles[0] = 0;
-    euler_angles[1] = 1;
-    euler_angles[2] = 0;
-    img1 = rotateX(img, intrinsic, euler_angles);
-    imwrite("../images/rotated_y.png", img1);
+//    // 欧拉角为RPY角（roll、pitch、yaw），即xyz顺序
+//    Vec3d euler_angles;
+//    euler_angles[0] = M_PI / 4;
+//    euler_angles[1] = 0;
+//    euler_angles[2] = 0;
+//    img1 = rotateX(img, intrinsic, euler_angles);
+//    imwrite("../images/rotated_x.png", img1);
+
+    double dis = 600;
+    Mat img2 = Mat(img.rows, img.cols, img.type(), (0, 0, 0));
+    trans1(img, img2, speckleInfo, dis);
+    imwrite("../images/trans_600.png", img2);
     return 0;
 }
