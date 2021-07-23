769af844bb2a9115ac164aface873494b23a899e
tianering <Tianering@yeah.net>
2021-07-23 11:09:35 +0800

    主类_调用平移类，获取不同距离的图像

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/calib3d.hpp>
+#include <iomanip>
 
 #include "objectIR.h"
 #include "rolling.h"
@@ -17,22 +18,24 @@ int main() {
     Intrinsic intrinsic;
     // 读取参数文件，获取相机内参等参数
     readTxt(speckleInfo, structureInfo, sensorInfo, intrinsic, path);
-    cout << speckleInfo.distance << endl;
     Vec3d translation;
     // 平移向量
-    Mat img = imread("../images/full_fov_speckle.bmp");
-    Mat img1 = Mat(img.rows, img.cols, CV_16FC3, Scalar(0, 0, 0));
-//    // 欧拉角为RPY角（roll、pitch、yaw），即xyz顺序
-//    Vec3d euler_angles;
-//    euler_angles[0] = M_PI / 4;
-//    euler_angles[1] = 0;
-//    euler_angles[2] = 0;
-//    img1 = rotateX(img, intrinsic, euler_angles);
-//    imwrite("../images/rotated_x.png", img1);
+    Mat img = imread("../images/trans_400.png");
+    Mat img1 = Mat(img.rows, img.cols, img.type(), Scalar(0, 0, 0));
+    // 欧拉角为RPY角（roll、pitch、yaw），即xyz顺序
+    for (int a = 1; a < 6; a++) {
+        Vec3d euler_angles;
+        euler_angles[0] = 0;
+        euler_angles[1] = 0;
+        euler_angles[2] = M_PI / 180 * a * 0.3;
+        rotateX(img, img1, euler_angles);
+        path = "../images/400_z" + to_string(a) + ".png";
+        imwrite(path, img1);
+    }
 
-    double dis = 600;
-    Mat img2 = Mat(img.rows, img.cols, img.type(), (0, 0, 0));
-    trans1(img, img2, speckleInfo, dis);
-    imwrite("../images/trans_600.png", img2);
-    return 0;
+//    double dis = 400;
+//    Mat img_out = Mat(sensorInfo.height, sensorInfo.width, img.type(), Scalar(0, 0, 0));
+//    trans1(img, img_out, intrinsic, speckleInfo, structureInfo, sensorInfo, dis);
+//    imwrite("../images/trans_400.png", img_out);
+//    return 0;
 }
